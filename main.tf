158668188f60f4004d3ca16e8e62c1df66f5d694
hweeks <harold.weeks@devoteam.com>
2023-03-24 19:31:58 +0100

    Primer commit

---
+++ main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.58.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = "us-central1"
+}
+
+
+resource "google_compute_instance" "terraform" {
+  project      = var.project_id
+  name         = var.instance_name
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_storage_bucket" "bucket" {
+  name          = var.bucket_name
+  location      = "US"
+  storage_class = var.storage_class
+  force_destroy = true
+
+  public_access_prevention = "enforced" 
+
+}
+
+resource "google_bigquery_dataset" "default" {
+  dataset_id                  = "tfprueba"
+  friendly_name               = "test-terraform"
+  description                 = "This is a test description"
+  location                    = "US"
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "project_id" {
+  description = "The project ID to host the network in"
+  type = string
+}
+
+variable "instance_name" {
+  type = string
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "storage_class" {
+  type = string
+}
+
+variable "dataset_id" {
+  type = string
+  
+}
